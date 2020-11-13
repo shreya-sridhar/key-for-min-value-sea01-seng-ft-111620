8999333eed0debec75600037d6cf47b82bbf1ce1
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 02:06:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value=
+  min_value = INFINITY
   hash.each do |key,value|
-    
-  
+    if min_value < value:
+      min_value = value 
+      min_key = key 
 end
