3dcfd9da6ca02625a1474f46098f6239500bea66
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 02:08:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_value = Float::INFINITY
-  hash.each do |key,value|
+  name_hash.each do |key,value|
     if min_value < value
       min_value = value 
       min_key = key 
