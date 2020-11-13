42a7335439b2e1b660785d21d1b33d66d333e881
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-13 02:06:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,10 @@
 def key_for_min_value(name_hash)
   min_value = INFINITY
   hash.each do |key,value|
-    if min_value < value:
+    if min_value < value
       min_value = value 
       min_key = key 
+    end
+  end
+  min_key
 end
